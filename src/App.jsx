e5f8f873b247972eae310cafdb9c316e006bf837
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2025-06-14 14:08:09 +0530

    Install router, icons, axios, and toast packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import { Toaster } from 'react-hot-toast';
 
 function App() {
 
   return (
     <>
+    <Toaster />
       <h1 className='text-3xl font-bold underline text-blue-600'>Hello Tailwind! test</h1>
     </>
   )
